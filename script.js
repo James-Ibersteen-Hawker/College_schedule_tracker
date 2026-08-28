eda7c54e534c2362a3ca2b38318c078ef98355c3
James Hawker <181120354+James-Ibersteen-Hawker@users.noreply.github.com>
2026-08-28 15:53:59 +0000

    preliminary if chain

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const buildings = document.querySelector("#buildings");
 const start = document.querySelector("#start");
 const end = document.querySelector("#end");
 const days = Array.from(document.querySelectorAll(".days"))
-
 const timeupdatedelay = 5000;
+const TIMEKEY = "time";
+const timeOpened = new Date();
+const dayOpened = timeOpened.getDay();
+let timeNow;
 function SAVE(event) {
     event.preventDefault();
     const RESPONSE = {
@@ -32,14 +35,34 @@ async function optionpopulate() {
     })
 }
 async function startup() {
-    await optionpopulate()
+    await optionpopulate();
+    const savedTime = localStorage.getItem(TIMEKEY);
+    if (!savedTime) {
+        //no saved time! Must be first opening
+        //send worker message to load the sheet
+    } else {
+        const convert = Number(savedTime);
+        if (convert !== dayOpened) { //different day
+            console.log("new day", convert, dayOpened);
+            //load sheet
+        } else { //same day
+            console.log("same day");
+            //load sheet from local storage, or else load new
+        }
+    }
+    localStorage.setItem(TIMEKEY, dayOpened); //save the new time
     form.addEventListener("submit", SAVE);
     CLOCK();
 }
 function CLOCK() {
     const clockinterval = setInterval(() => {
         console.log("clock");
-        const timenow = new Date();
+        ping();
     }, timeupdatedelay)
+}
+function ping() { //every clock cycle, the website performs a "ping" to see if the time / schedule need to change
+    const timeNow = new Date();
+    const dayNow = timeNow.getDay();
+    if (dayNow !== dayOpened) {/* refresh schedule */}
 }
 startup();
