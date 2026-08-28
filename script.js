6c51042a1c845186b467faf2cf2e7d1a48d2ea12
James Hawker <181120354+James-Ibersteen-Hawker@users.noreply.github.com>
2026-08-28 13:29:03 +0000

    added days and times checkboxes

--- script.js
+++ script.js
@@ -4,34 +4,43 @@ const name = document.querySelector("#name");
 const room = document.querySelector("#room");
 const teacher = document.querySelector("#teacher");
 const buildings = document.querySelector("#buildings");
+const start = document.querySelector("#start");
+const end = document.querySelector("#end");
+const days = Array.from(document.querySelectorAll(".days"))
+
 const timeupdatedelay = 5000;
 function temp(event) {
     event.preventDefault();
+    console.log(days)
     const RESPONSE = {
         name: name.value,
         room: room.value,
         teacher: teacher.value,
-        building: buildings.value
+        building: buildings.value,
+        start: start.value,
+        end: end.value,
+        days: days.filter(e => e.checked).map(e => e.value)
     }
     console.log(RESPONSE)
 }
-async function startup() {
+async function optionpopulate() {
     const classlist = await (await fetch("./classlist.json")).json();
     classlist.forEach(e => {
-        console.log(e);
         const option = document.createElement("option");
         option.value = e.number;
         option.textContent = e.name;
         buildings.append(option)
     })
+}
+async function startup() {
+    await optionpopulate()
     form.addEventListener("submit", temp);
     CLOCK();
 }
-startup();
-
 function CLOCK() {
     const clockinterval = setInterval(() => {
         console.log("clock");
         const timenow = new Date();
     }, timeupdatedelay)
 }
+startup();
